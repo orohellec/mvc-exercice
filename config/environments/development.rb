6e23e05293c18b49129da137d9d17c7781b889e6
Olivier <orohellec44@gmail.com>
2019-01-16 15:03:17 +0100

    maildev in development is working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
   # Default url option for the devise mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :address => "localhost",
+        :port => 1030
+    }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
